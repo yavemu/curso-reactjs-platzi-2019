9db931ba105df5bdad324630b8c9c17946d610fd
Yamid V.M <yavemu@gmail.com>
2019-04-10 14:20:42 -0500

    Ajustes BadgesList

--- src/components/BadgesList/BadgesList.js
+++ src/components/BadgesList/BadgesList.js
@@ -8,14 +8,14 @@ class BadgesList extends React.Component {
             <ul className="BadgesList list-unstyled">
                 {this.props.badges.map((badge) => {
                     return (
-                        <li className="BadgesList__Badge shadow-sm p-2 m-3 row align-items-center" key={badge.id}>
-                            <img src={badge.avatarUrl} alt="Avatar" className="col-3 img-thumbnail img-fluid"/>
-                            <div className="col-7">
+                        <li className="BadgesList__Badge shadow rounded p-2 m-3 row align-items-center" key={badge.id}>
+                            <img src={badge.avatarUrl} alt="Avatar" className="sm img-fluid rounded-circle"/>
+                            <div className="col">
                                 <b className="BadgeList__Badge-name">{badge.firstName} {badge.lastName}</b>
                                 <div className="BadgeList__Badge-jobTitle">{badge.jobTitle}</div>
                                 <div className="BadgeList__Badge-twitter">
                                     <img src={twitterLogo} alt="Twitter Logo"/>
-                                    <small> {badge.twitter}</small>
+                                    <small> @{badge.twitter}</small>
                                 </div>
                             </div>
                         </li>
