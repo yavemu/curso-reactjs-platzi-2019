4665dff68bf88f5bd1cb5ec466e5c789bdb07ef1
Yamid V.M <yavemu@gmail.com>
2019-04-11 21:37:54 -0500

    Consumiendo API + List data + Empty data + Error  Api

--- src/pages/Badges/Badges.js
+++ src/pages/Badges/Badges.js
@@ -2,76 +2,42 @@ import React from "react";
 import BadgesList from "../../components/BadgesList/BadgesList";
 import confLogo from "../../images/badge-header.svg";
 import { Link } from "react-router-dom";
+import api from "../../api";
 
 import './Badges.css';
 
 class Badges extends React.Component {
-
-    constructor(props){
-        console.log("1. constructor()");
-        
-        super(props);
-        this.state = {
-            data: [],
-        }
-
+    state = {
+        loading: true,
+        error: null,
+        data: undefined
     }
 
     componentDidMount(){
-        console.log("3. componentDidMount()");
-
-        this.timeOut = setTimeout(() => {
-            this.setState({
-                data: [
-                    {
-                        id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-                        irstName: "Freda",
-                        lastName: "Grady",
-                        email: "Leann_Berge@gmail.com",
-                        jobTitle: "Legacy Brand Director",
-                        twitter: "FredaGrady22221-7573",
-                        avatarUrl: "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
-                    },
-                    {
-                        id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-                        firstName: "Major",
-                        lastName: "Rodriguez",
-                        email: "Ilene66@hotmail.com",
-                        jobTitle: "Human Research Architect",
-                        twitter: "ajorRodriguez61545",
-                        avatarUrl: "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon"
-                    },
-                    {
-                        id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-                        firstName: "Daphney",
-                        lastName: "Torphy",
-                        email: "Ron61@hotmail.com",
-                        jobTitle: "National Markets Officer",
-                        twitter: "DaphneyTorphy96105",
-                        avatarUrl: "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon"
-                    }
-                ]
-            }) 
-        }, 3000);
+        this.fetchGetData();
     }
 
-    componentDidUpdate(prevProps, prevState){
-        console.log("5. componentDidUpdate()");
-        console.log('props',{
-            prevProps: prevProps, this_props: this.props,
-        });
-        console.log('state',{
-            prevState: prevState, this_state: this.state,
-        });
+    fetchGetData = async () => {
+        this.setState({ loading: true, error: null });
+        
+        try {
+            const data= await api.badges.list();
+            this.setState({ loading: false, data });
+        } catch (error) {
+            this.setState({ loading: false, error });
+        }
     }
-
-    componentWillUnmount(){
-        console.log("6. componentWillUnmount()");
-        clearTimeout(this.timeOut);
-    }
-
+    
     render(){
-        console.log("2-4. render()");
+            if (this.state.error) {
+                return `Ups!!!. There are an error ${this.state.error.message}`
+            }
+            
+            if (this.state.loading) {
+                return 'Loading...'
+            }
+
+            
         return <React.Fragment>
             <div className="Badges">
                 <div className="Badges__hero">
@@ -82,17 +48,22 @@ class Badges extends React.Component {
             </div>
 
             <div className="Badges__container col align-self-center">
-                <div className="Badges__buttons">
-                    <Link to="/badges/new" className="btn btn-primary">
-                        New Badge
-                    </Link>
+                <div className="Badges__container">
+                    {!this.state.data.length && 
+                        <div class="alert alert-danger" role="alert">No badges were found.</div>
+                    }
+                    <div className="Badges__buttons">
+                        <Link to="/badges/new" className="btn btn-primary">
+                            Create new Badge
+                        </Link>
+                    </div>
                 </div>
 
-                <div className="Badges_list col align-self-center">
+                {!!this.state.data.length && <div className="Badges_list col align-self-center">
                     <div className="Badges__container">
                         <BadgesList badges={this.state.data} />
                     </div>
-                </div>
+                </div>}
             </div>
         </React.Fragment>
     }
