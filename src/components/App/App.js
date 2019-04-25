2f2bf2805e8892ec5eb40517e4cdfa01423439c1
Yamid V.M <yavemu@gmail.com>
2019-04-25 16:05:29 -0500

    Detalles de los badges + nuevo componente

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Layout from '../Layout/Layout';
 // PAGES
 import Badges from "../../pages/Badges/Badges";
 import BadgeNew from "../../pages/BadgeNew/BadgeNew";
+import BadgeDetails from "../../pages/BadgeDetails/BadgeDetails";
 import BadgeEdit from "../../pages/BadgeEdit/BadgeEdit";
 import NotFound from "../../pages/NotFound/NotFound";
 import Home from "../../pages/Home/Home";
@@ -17,6 +18,7 @@ function App() {
                     <Route exact path="/" component={Home} />
                     <Route exact path="/badges" component={Badges} />
                     <Route exact path="/badges/new" component={BadgeNew} />
+                    <Route exact path="/badges/:badgeId" component={BadgeDetails} />
                     <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
                     <Route component={NotFound} />
                 </Switch>
--- src/components/BadgesList/BadgesList.js
+++ src/components/BadgesList/BadgesList.js
@@ -3,23 +3,32 @@ import "./BadgesList.css";
 import twitterLogo from "../../images/twitter_logo.png";
 import { Link } from "react-router-dom";
 
+class BadgesListItem extends React.Component {
+    render() {
+        return (
+            <div className="BadgesListItem">
+                <img src={this.props.badge.avatarUrl} alt="Avatar" className="sm img-fluid rounded-circle" />
+                <div className="col">
+                    <b className="BadgeList__Badge-name">{this.props.badge.firstName} {this.props.badge.lastName}</b>
+                    <div className="BadgeList__Badge-jobTitle">{this.props.badge.jobTitle}</div>
+                    <div className="BadgeList__Badge-twitter">
+                        <img src={twitterLogo} alt="Twitter Logo" />
+                        <small> @{this.props.badge.twitter}</small>
+                    </div>
+                </div>
+            </div> 
+        )}
+}
+
 class BadgesList extends React.Component {
     render() {
         return (
             <ul className="BadgesList list-unstyled">
                 {this.props.badges.map((badge) => {
                     return (
-                        <li className="BadgesList__Badge shadow rounded m-3 row align-items-center" key={badge.id}>
-                            <Link to={`/badges/${badge.id}/edit`} className='text-reset text-decoration-none row m-2'>
-                                <img src={badge.avatarUrl} alt="Avatar" className="sm img-fluid rounded-circle" />
-                                <div className="col">
-                                    <b className="BadgeList__Badge-name">{badge.firstName} {badge.lastName}</b>
-                                    <div className="BadgeList__Badge-jobTitle">{badge.jobTitle}</div>
-                                    <div className="BadgeList__Badge-twitter">
-                                        <img src={twitterLogo} alt="Twitter Logo" />
-                                        <small> @{badge.twitter}</small>
-                                    </div>
-                                </div>
+                        <li className="BadgesList__Badge rounded m-3 row align-items-center" key={badge.id}>
+                            <Link to={`/badges/${badge.id}`} className='text-reset text-decoration-none'>
+                                <BadgesListItem badge={badge} />
                             </Link>
                         </li>
                     )
