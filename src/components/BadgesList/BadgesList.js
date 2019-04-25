9d6fa3151af952c64186a8cdd79f820c7d5dbbc5
Yamid V.M <yavemu@gmail.com>
2019-04-25 11:48:42 -0500

    Editar un badge + nuevo componente BadgeEdit

--- src/components/BadgesList/BadgesList.js
+++ src/components/BadgesList/BadgesList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./BadgesList.css";
 import twitterLogo from "../../images/twitter_logo.png";
+import { Link } from "react-router-dom";
 
 class BadgesList extends React.Component {
     render() {
@@ -8,16 +9,18 @@ class BadgesList extends React.Component {
             <ul className="BadgesList list-unstyled">
                 {this.props.badges.map((badge) => {
                     return (
-                        <li className="BadgesList__Badge shadow rounded p-2 m-3 row align-items-center" key={badge.id}>
-                            <img src={badge.avatarUrl} alt="Avatar" className="sm img-fluid rounded-circle"/>
-                            <div className="col">
-                                <b className="BadgeList__Badge-name">{badge.firstName} {badge.lastName}</b>
-                                <div className="BadgeList__Badge-jobTitle">{badge.jobTitle}</div>
-                                <div className="BadgeList__Badge-twitter">
-                                    <img src={twitterLogo} alt="Twitter Logo"/>
-                                    <small> @{badge.twitter}</small>
+                        <li className="BadgesList__Badge shadow rounded m-3 row align-items-center" key={badge.id}>
+                            <Link to={`/badges/${badge.id}/edit`} className='text-reset text-decoration-none row m-2'>
+                                <img src={badge.avatarUrl} alt="Avatar" className="sm img-fluid rounded-circle" />
+                                <div className="col">
+                                    <b className="BadgeList__Badge-name">{badge.firstName} {badge.lastName}</b>
+                                    <div className="BadgeList__Badge-jobTitle">{badge.jobTitle}</div>
+                                    <div className="BadgeList__Badge-twitter">
+                                        <img src={twitterLogo} alt="Twitter Logo" />
+                                        <small> @{badge.twitter}</small>
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
                         </li>
                     )
                 })}
---
+++ src/pages/BadgeEdit/BadgeEdit.js
@@ -0,0 +1,106 @@
+import React from "react";
+import Badge from '../../components/Badge/Badge';
+import BadgeForm from '../../components/BadgeForm/BadgeForm';
+import md5 from 'md5';
+
+import header from '../../images/platziconf-logo.svg';
+import './BadgeEdit.css';
+
+import api from "../../api";
+import LoadingAnimation from "../../components/LoadingAnimation/LoadingAnimation";
+
+class BadgeEdit extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true,
+            error: null,
+            form: {
+                email: '',
+                firstName: '',
+                jobTitle: '',
+                lastName: '',
+                twitter: '',
+                avatarUrl: ''
+            }
+        }
+
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    componentDidMount(){
+        this.fetchData();
+    }
+
+    fetchData = async e => {
+        this.setState({loading:true, error:null});
+        
+        try {
+            const data = await api.badges.read(
+                this.props.match.params.badgeId
+            );            
+            this.setState({loading:false, form:data});
+        } catch (error) {
+            this.setState({loading:false, error:error});
+        }
+    }
+
+    handleOnChange = e => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        this.setState({ loading:true, error:null });
+        if (this.state.form.email) {
+            const createAvatarUrl = `https://www.gravatar.com/avatar/${md5(this.state.form.email)}?d=identicon`;
+            this.state.form.avatarUrl = createAvatarUrl;
+        }
+        
+        try {
+            await api.badges.create(this.state.form);
+            this.setState({ loading:false });
+            this.props.history.push("/badges");
+        } catch (error) {
+            this.setState({ loading:true, error });
+        }
+    }
+
+    render () {
+        if (this.state.loading && !this.state.error) {
+            return <LoadingAnimation/>
+        }
+        return <React.Fragment>
+            <div className="BadgeEdit__hero">
+                <img className="image-fluid BadgeEdit__hero-image" src={header} alt="Logo"/>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <Badge {...this.state.form} />
+                    </div>
+
+                    <div className="col-6">
+                        <BadgeForm 
+                            onChange={this.handleOnChange} 
+                            onSubmit={this.handleSubmit} 
+                            {...this.state.form} 
+                            error={this.state.error}
+                            />
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    }
+}
+
+export default BadgeEdit;
