6556c846e033244fd6316dc320130881c04d400e
Yamid V.M <yavemu@gmail.com>
2019-05-01 21:36:36 -0500

    Usar Hooks de react para filtrar Badges

--- src/components/BadgesList/BadgesList.js
+++ src/components/BadgesList/BadgesList.js
@@ -20,13 +20,69 @@ class BadgesListItem extends React.Component {
         )}
 }
 
-class BadgesList extends React.Component {
-    render() {
-        return (
-            <ul className="BadgesList list-unstyled">
-                {this.props.badges.map((badge) => {
+function BadgesList (props) {
+    const badges = props.badges;
+    
+    const [query, setQuery] = React.useState('');
+    
+    // const filterBadges = badges.filter((badge)=>{
+    //     return `${badge.firstName} ${badge.lastName}`
+    //         .toLowerCase()
+    //         .includes(query.toLowerCase())
+    // });
+
+
+    const [filterBadges, setFilterBadges] = React.useState(badges);
+
+    React.useMemo(
+        () => {
+            const result = badges.filter((badge) => {
+                return `${badge.firstName} ${badge.lastName}`
+                    .toLowerCase()
+                    .includes(query.toLowerCase())
+            });
+
+            setFilterBadges(result)
+        }
+    ,[badges, query])
+
+
+    if (filterBadges.length === 0) {
+        return(
+            <div>
+                <div className="form-group">
+                    <label>Filter Badges</label>
+                    <input type="text" className='form-control'
+                        value={query}
+                        onChange={((e) => {
+                            setQuery(e.target.value)
+                        })}
+                    />
+                </div>
+                <h3>No badges were found</h3>
+                <Link to='/badges/new' className='btn btn-primary'>
+                    Create new badge
+                </Link>
+            </div>
+        )
+    }
+    
+    return (
+        <div className="BadgesList">
+            <div className="form-group">
+                <label>Filter Badges</label>
+                <input type="text" className='form-control'
+                    value={query}
+                    onChange={((e) => {
+                        setQuery(e.target.value)
+                    })}
+                />
+            </div>
+        
+            <ul className="list-unstyled">
+                {filterBadges.map((badge) => {
                     return (
-                        <li className="BadgesList__Badge rounded m-3 row align-items-center" key={badge.id}>
+                        <li className="BadgesList__Badge rounded align-items-center" key={badge.id}>
                             <Link to={`/badges/${badge.id}`} className='text-reset text-decoration-none'>
                                 <BadgesListItem badge={badge} />
                             </Link>
@@ -34,8 +90,9 @@ class BadgesList extends React.Component {
                     )
                 })}
             </ul>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default BadgesList;
